0c2e424da24a1aa1294514d527bc26543eb7712f
yo-goto <50942816+yo-goto@users.noreply.github.com>
2022-02-21 17:13:42 +0900

    added completions

---
+++ completions/fish-color.fish
@@ -0,0 +1,3 @@
+complete -c fish-color -s v -l version -f -d "Show version info"
+complete -c fish-color -s h -l help -f -d "Show help"
+complete -c fish-color -s p -l print -f -d "Show a list of the 16 named colors"
