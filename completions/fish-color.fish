bf19e20b705755eb8651dcb9c82779bbf5921bf2
yo-goto <50942816+yo-goto@users.noreply.github.com>
2022-02-21 17:46:52 +0900

    updated completions: removed files suggetion

--- completions/fish-color.fish
+++ completions/fish-color.fish
@@ -1,3 +1,4 @@
+complete -c fish-color --no-files
 complete -c fish-color -s v -l version -f -d "Show version info"
 complete -c fish-color -s h -l help -f -d "Show help"
 complete -c fish-color -s p -l print -f -d "Show a list of the 16 named colors"
